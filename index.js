9fb15daa935badb9da65770b3fea386e3f380fa7
Aravindan E <129530507+Aravindan-14@users.noreply.github.com>
2024-01-24 07:41:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,119 @@
+//      JavaScript Variables
+
+//           var 
+//           let 
+//           const  
+
+
+//           Operators
+
+//         Arithmetic Operators
+
+//         +	Addition
+//         -	Subtraction
+//         *	Multiplication
+//         /	Division
+//         %	Modulus (Division Remainder)
+//         ++	Increment
+//         --	Decrement
+
+
+//              Operators
+
+//              Assignment Operators
+
+//              Operator	Example	      Same As
+
+//                =	         x = y	      x = y
+//                +=	     x += y	      x = x + y
+//                -=	     x -= y	      x = x - y
+
+
+//               Comparison Operators
+
+                
+//                Operator	Description
+
+//                ==	         equal to
+//                ===	         equal value and equal type
+//                !=	         not equal
+//                !==	         not equal value or not equal type
+//                >	         greater than
+//                <	         less than
+//                >=	         greater than or equal to
+//                <=	         less than or equal to
+
+
+//                Datatypes
+
+//                1. String
+//                2. Number
+//                3. Boolean
+
+
+//                functions
+
+//                function myFunction(a, b) {
+//                Function returns the product of a and b
+//                  return a * b;
+//                }
+
+//                let x = myFunction(4, 3);
+
+//                console.log(x);
+
+
+//                JavaScript Objects
+
+//                const car = {type:"Fiat", model:"500", color:"white"};
+
+
+//                Events 
+
+//                Array
+
+//               Array push()
+//               Array pop()
+//               Array length
+//               Array shift()
+//               Array unshift()
+
+
+
+//                JavaScript For Loop
+
+//                for (let i = 0; i < 5; i++) {
+//                    text += "The number is " + i + "<br>";
+//                  }
+
+
+//                 JavaScript if, else, and else if
+
+//                if (time < 10) {
+//                    greeting = "Good morning";
+//                  } else if (time < 20) {
+//                    greeting = "Good day";
+//                  } else {
+//                    greeting = "Good evening";
+//                  }
+                  
+
+
+let id= "b.com"
+let password=123
+
+let userNameInput = document.getElementById("userName").value 
+
+let passwordInput= document.getElementById("password").value
+
+
+function check(){
+    console.log(userNameInput);
+    console.log(passwordInput);
+  
+}
+
+
+// github
+
+https://github.com/Aravindan-14/day1-Task.git
